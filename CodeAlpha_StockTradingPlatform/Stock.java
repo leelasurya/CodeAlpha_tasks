f87b2ac1b0677b13c1e108f1f3afac38d831e662
leelasurya <leelasurya765@gmail.com>
2026-09-05 17:19:24 +0530

    Create Stock.java

---
+++ CodeAlpha_StockTradingPlatform/Stock.java
@@ -0,0 +1,30 @@
+public class Stock {
+
+    private String symbol;
+    private String name;
+    private double price;
+
+    public Stock(String symbol, String name, double price) {
+        this.symbol = symbol;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayStock() {
+        System.out.println(
+            symbol + " - " + name + " | Price: $" + price
+        );
+    }
+}
